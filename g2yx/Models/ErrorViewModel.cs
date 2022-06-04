ebfe765b41308a1c5b029803f4350289e8dfbc8d
Max Tiutmanov <max.tiutmanov@bookedby.com>
2022-06-04 11:29:56 +0300

    initial commit

---
+++ g2yx/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace g2yx.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
